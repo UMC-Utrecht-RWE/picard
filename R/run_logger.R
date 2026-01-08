b708cffd919e516d6b5c6753e0d93197daf03ad7
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2026-01-08 18:24:27 +0100

    testing new start_capturing_prints

--- R/run_logger.R
+++ R/run_logger.R
@@ -273,19 +273,19 @@ LoggerManager <- R6::R6Class( # nolint
       scr_txt <- if (is.na(script_s)) "NA" else base::sprintf("%.2f", script_s)
 
 
-    #   base::sprintf(
-    #     "%s | %-5s | run+%8.2fs | step+%8ss | scr+%8ss | d+%7.2fs | %s/%s | %s",
-    #     base::format(now, "%Y-%m-%d %H:%M:%S"),
-    #     lvl_txt,
-    #     run_s,
-    #     step_txt,
-    #     scr_txt,
-    #     delta_s,
-    #     step,
-    #     scr,
-    #     message
-    #   )
-    # },
+      #   base::sprintf( # Original with step and script times
+      #     "%s | %-5s | run+%8.2fs | step+%8ss | scr+%8ss | d+%7.2fs | %s/%s | %s", # nolint
+      #     base::format(now, "%Y-%m-%d %H:%M:%S"), # nolint
+      #     lvl_txt,
+      #     run_s,
+      #     step_txt,
+      #     scr_txt,
+      #     delta_s,
+      #     step,
+      #     scr,
+      #     message
+      #   )
+      # },
 
       base::sprintf(
         "%s | %-5s | run+%8.2fs | scr+%8ss | %s/%s | %s",
@@ -303,11 +303,34 @@ LoggerManager <- R6::R6Class( # nolint
     #'
     #' Redirects all `stdout` output (e.g., print statements)
     #' to the global log file.
-    #'
+    #' @param target Target log file to capture prints.
+    #'  Options are "global" or "step".
+    #' @param capture_messages Whether to also capture messages/warnings.
     #' @return None
-    start_capturing_prints = function() {
-      sink_file <- self$step_log_file
+    start_capturing_prints = function(
+      target = c("global", "step"),
+      capture_messages = TRUE
+    ) {
+      target <- base::match.arg(target)
+
+      sink_file <- if (target == "global") {
+        self$global_log_file
+      } else {
+        self$step_log_file
+      }
+
+      if (base::is.null(sink_file)) {
+        stop("No log file available for target: ", target)
+      }
+
+      # stdout (cat/print)
       base::sink(sink_file, append = TRUE, type = "output")
+
+      # message()/warning()/package startup messages (usually stderr-ish)
+      if (isTRUE(capture_messages)) {
+        base::sink(sink_file, append = TRUE, type = "message")
+      }
+
       invisible(NULL)
     },
 
@@ -317,7 +340,13 @@ LoggerManager <- R6::R6Class( # nolint
     #'
     #' @return None
     stop_capturing_prints = function() {
-      base::sink(type = "output")
+      # close message sink first if open
+      while (base::sink.number(type = "message") > 0) {
+        base::sink(type = "message")
+      }
+      while (base::sink.number(type = "output") > 0) {
+        base::sink(type = "output")
+      }
       invisible(NULL)
     }
   )
