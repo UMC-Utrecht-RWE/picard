f6ecec43bcca522b0d323499494d20752ca69416
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2026-01-15 16:16:17 +0100

    renamed read config (#38)
    * renamed because now it loads more the one congif at the time
    
    * updating man

--- R/utils.R
+++ R/utils.R
@@ -12,7 +12,7 @@
 #'
 #' @return A list of configuration values.
 #' @export
-load_config_values <- function(file_path = NULL) {
+load_config <- function(file_path = NULL) {
   if (!base::is.null(file_path)) {
     if (base::file.exists(file_path)) {
       read_yaml(file_path)
