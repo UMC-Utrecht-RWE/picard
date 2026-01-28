ff6dd50f654a1ed1db262f0c279e652e31b4c475
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2026-01-28 10:15:37 +0100

    moved test to cerrect file

--- tests/testthat/test_read_data.R
+++ tests/testthat/test_read_data.R
@@ -243,6 +243,19 @@ testthat::test_that("tests for define_column_types", {
   )
 })
 
+testthat::test_that("Changing velue of some columns", {
+  df <- create_test_data()
+  df <- define_column_types(df = df, col_types = c(
+    date = "character",
+    logi = as.logical,
+    chr = as.factor
+  ))
+  testthat::expect_true(is.character(df$date))
+  testthat::expect_true(is.logical(df$logi))
+  testthat::expect_true(is.factor(df$chr))
+})
+
+
 ###############################
 # Tests for .init_reader_registry
 ###############################
