7d9eb0045db054ea41eacf6ce4d13736c5477893
Mattia Cinelli <m.cinelli@umcutrecht.nl>
2026-02-03 15:58:40 +0100

    some details need fixing

--- tests/testthat/test_delete_data.R
+++ tests/testthat/test_delete_data.R
@@ -78,6 +78,18 @@ testthat::test_that("Test delete_paths with paths", {
   file_1 <- fs::file_create(fs::path(temp_dir, "file_1.txt"))
   file_2 <- fs::file_create(fs::path(temp_dir, "file_2.txt"))
 
+  testthat::expect_error(
+    delete_paths(),
+    regexp = "`paths` is required.",
+    fixed = TRUE
+  )
+
+  testthat::expect_error(
+    delete_paths(list(1, 2)),
+    regexp = "`paths` must be a character vector or list of character.",
+    fixed = TRUE
+  )
+
   testthat::expect_message(
     delete_paths(
       paths = list(file_1, file_2),
@@ -118,4 +130,25 @@ testthat::test_that("Test delete_paths with dirs", {
 
   testthat::expect_false(fs::file_exists(file_1))
   testthat::expect_false(fs::file_exists(file_2))
+  testthat::expect_false(fs::dir_exists(temp_dir))
 })
+
+# testthat::test_that("Test delete_paths with dirs", {
+#   temp_dir <- withr::local_tempdir()
+#   file_1 <- fs::file_create(fs::path(temp_dir, "file_1.txt"))
+#   file_2 <- fs::file_create(fs::path(temp_dir, "file_2.txt"))
+
+#   testthat::expect_message(
+#     delete_paths(
+#       paths = list(temp_dir, file_1, file_2),
+#       dry_run = FALSE,
+#       del_dir = FALSE
+#     ),
+#     regexp = "Deleted the following paths:",
+#     fixed = TRUE
+#   )
+
+#   testthat::expect_false(fs::file_exists(file_1))
+#   testthat::expect_false(fs::file_exists(file_2))
+#   testthat::expect_true(fs::dir_exists(temp_dir))
+# })
