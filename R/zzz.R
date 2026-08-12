0d8ba1d675c52d6e4c0b0f2dcdfb4c097aacaf82
Test <test@example.com>
2026-08-12 09:01:49 +0200

    fixing missing globals

--- R/zzz.R
+++ R/zzz.R
@@ -23,12 +23,14 @@
 #' @keywords internal
 utils::globalVariables(c(
   # data.table's non-standard evaluation
-  ".",      # data.table's .() syntax
-  ".SD",    # Subset of Data
-  ".N",     # Number of rows
-  ".I",     # Row indices
-  ".GRP",   # Group counter
-  ".BY",    # List of by values
+  ".", # data.table's .() syntax
+  ".SD", # Subset of Data
+  ".N", # Number of rows
+  ".I", # Row indices
+  ".GRP", # Group counter
+  ".BY", # List of by values
+  "min_start",
+
 
   # Add unquoted column names here if needed:
   "alert_summary",
